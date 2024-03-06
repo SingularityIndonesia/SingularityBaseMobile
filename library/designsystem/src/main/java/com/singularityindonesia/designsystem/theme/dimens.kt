913d17fba41ef6a53642e38d5c82a51614d86cdc
stefanus ayudha <stefanus.ayudha@gmail.com>
2024-03-06 09:08:25 +0700

    feat(DesignSystem): Add dimens.

---
+++ library/designsystem/src/main/java/com/singularityindonesia/designsystem/theme/dimens.kt
@@ -0,0 +1,15 @@
+package com.singularityindonesia.designsystem.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+val SmallSpacing = 4.dp
+val MediumSpacing = 8.dp
+val LargeSpacing = 16.dp
+val ExtraLargeSpacing = 24.dp
+
+
+val SmallPadding = 4.dp
+val MediumPadding = 8.dp
+val LargePadding = 16.dp
+val ExtraLargePadding = 24.dp
