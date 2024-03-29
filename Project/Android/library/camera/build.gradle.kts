d642a44ea7a8841754c258eee03cdc4656174f96
stefanus ayudha <stefanus.ayudha@gmail.com>
2024-03-30 02:35:05 +0700

    chore: add camera library.

---
+++ Project/Android/library/camera/build.gradle.kts
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2024 Stefanus Ayudha (stefanus.ayudha@gmail.com)
+ * Created on 04/03/2024 12:00
+ * You are not allowed to remove the copyright.
+ */
+plugins {
+    id("LibraryConventionV1")
+    id("FeatureJetpackCompose")
+    id("FeatureContextReceiver")
+}
+
+android {
+    namespace = "com.singularityindonesia.camera"
+}
+
+dependencies {
+
+    implementation(project(":library:permission"))
+
+    implementation(libs.android.core.ktx)
+    implementation(libs.android.appcompat)
+    implementation(libs.android.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.android.ext.junit)
+    androidTestImplementation(libs.android.espresso.core)
+
+    implementation(libs.android.core.ktx)
+    implementation(libs.android.appcompat)
+    implementation(libs.android.material)
+
+    implementation(libs.android.compose.ui)
+    implementation(libs.android.compose.ui.graphics)
+    implementation(libs.android.compose.ui.tooling.preview)
+    implementation(libs.android.compose.material3)
+
+    implementation(libs.android.camera)
+    implementation(libs.android.camera.lifecycle)
+    implementation(libs.android.camera.view)
+
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.android.ext.junit)
+    androidTestImplementation(libs.android.espresso.core)
+}
