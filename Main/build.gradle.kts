d7c168927f2a35642b38c9946fecb481ef1f214e
stefanus ayudha <stefanus.ayudha@gmail.com>
2024-05-10 03:29:06 +0700

    chore: add missing Main project.

---
+++ Main/build.gradle.kts
@@ -0,0 +1,1 @@
+../build.gradle.template.kts
---
+++ Main/composeApp/src/commonMain/kotlin/App.kt
@@ -0,0 +1,48 @@
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import com.singularity.data.VmSuccess
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+import main.composeapp.generated.resources.Res
+import main.composeapp.generated.resources.compose_multiplatform
+
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+@Preview
+fun App() {
+    val greeting = remember { Greeting().greet() }
+    val testMultiProject = remember {
+        VmSuccess(greeting)
+    }
+    MaterialTheme {
+        var showContent by remember { mutableStateOf(false) }
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = { showContent = !showContent }) {
+                Text("Click me!")
+            }
+            AnimatedVisibility(showContent) {
+                Column(
+                    Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(painterResource(Res.drawable.compose_multiplatform), null)
+                    Text(
+                        "Test Multi Project:\n$greeting",
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+        }
+    }
+}
