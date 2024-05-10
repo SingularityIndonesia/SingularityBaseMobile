6b23722335b5bb245427c4d0ab884abdad82ef36
stefanus ayudha <stefanus.ayudha@gmail.com>
2024-05-10 13:23:36 +0700

    chore: update gradle build using AppConventionV1.

--- Main/composeApp/build.gradle.kts
+++ Main/composeApp/build.gradle.kts
@@ -1,33 +1,12 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
+    id("AppConventionV1")
     alias(libs.plugins.jetbrainsCompose)
 }
 
 kotlin {
-    androidTarget {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = "11"
-            }
-        }
-    }
-    
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "ComposeApp"
-            isStatic = true
-        }
-    }
-    
     sourceSets {
-        
         androidMain.dependencies {
             implementation(libs.compose.ui.tooling.preview)
             implementation(libs.androidx.activity.compose)
@@ -47,32 +26,10 @@ kotlin {
 
 android {
     namespace = "com.singularity.basemobile"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-
-    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-    sourceSets["main"].res.srcDirs("src/androidMain/res")
-    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
-
     defaultConfig {
         applicationId = "com.singularity.basemobile"
-        minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
     }
     dependencies {
         debugImplementation(libs.compose.ui.tooling)
--- Plugin/convention/src/main/kotlin/com/singularityindonesia/convention/AppConventionV1.kt
+++ Plugin/convention/src/main/kotlin/com/singularityindonesia/convention/AppConventionV1.kt
@@ -6,26 +6,22 @@
 package com.singularityindonesia.convention
 
 import com.android.build.api.dsl.ApplicationExtension
+import com.singularityindonesia.convention.companion.DefaultConfigs
 import com.singularityindonesia.convention.companion.DefaultConfigs.COMPILE_SDK
-import com.singularityindonesia.convention.companion.DefaultConfigs.EXCLUDED_RESOURCES
-import com.singularityindonesia.convention.companion.DefaultConfigs.JAVA_SOURCE_COMPAT
-import com.singularityindonesia.convention.companion.DefaultConfigs.JAVA_TARGET_COMPAT
-import com.singularityindonesia.convention.companion.DefaultConfigs.JVM_TARGET
 import com.singularityindonesia.convention.companion.DefaultConfigs.MIN_SDK
-import com.singularityindonesia.convention.companion.DefaultConfigs.PROGUARD_ANDROID_OPTIMIZE
-import com.singularityindonesia.convention.companion.DefaultConfigs.PROGUARD_RULES
-import com.singularityindonesia.convention.companion.DefaultConfigs.TEST_INST_RUNNER
-import com.singularityindonesia.convention.companion.getSigningConfig
-import com.singularityindonesia.convention.companion.kotlinCompile
+import com.singularityindonesia.convention.companion.DefaultConfigs.TARGET_SDK
+import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.get
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
 class AppConventionV1 : Plugin<Project> {
 
     private val PLUGINS = listOf(
-        "org.jetbrains.kotlin.android",
-        "com.android.application"
+        "com.android.application",
+        "org.jetbrains.kotlin.multiplatform"
     )
 
     override fun apply(target: Project) =
@@ -34,91 +30,51 @@ class AppConventionV1 : Plugin<Project> {
                 PLUGINS.forEach(::apply)
             }
 
-            extensions.configure<ApplicationExtension> {
-                compileSdk = COMPILE_SDK
-                defaultConfig {
-                    minSdk = MIN_SDK
-                    testInstrumentationRunner = TEST_INST_RUNNER
-                    vectorDrawables {
-                        useSupportLibrary = true
-                    }
-                }
-                buildTypes {
-                    val signingConfigAll = getSigningConfig(
-                        project = project,
-                        extension = this@configure
-                    )
-
-                    release {
-                        if (signingConfigAll != null)
-                            this.signingConfig = signingConfigAll
-
-                        isMinifyEnabled = true
-                        isDebuggable = false
-                        isJniDebuggable = false
-                        proguardFiles(
-                            getDefaultProguardFile(
-                                PROGUARD_ANDROID_OPTIMIZE
-                            ),
-                            PROGUARD_RULES
-                        )
-                    }
-
-                    debug {
-                        if (signingConfigAll != null)
-                            this.signingConfig = signingConfigAll
-
-                        isMinifyEnabled = false
-                        isDebuggable = true
-                        isJniDebuggable = true
-                        proguardFiles(
-                            getDefaultProguardFile(
-                                PROGUARD_ANDROID_OPTIMIZE
-                            ),
-                            PROGUARD_RULES
-                        )
-                    }
-                    create("devDebug") {
-                        initWith(getByName("debug"))
-                        isDefault = true
-                    }
-                    create("dev") {
-                        initWith(getByName("release"))
-                    }
-                    create("stagingDebug") {
-                        initWith(getByName("debug"))
-                    }
-                    create("staging") {
-                        initWith(getByName("release"))
-                    }
-                    create("prodDebug") {
-                        initWith(getByName("debug"))
-                    }
-                    create("prod") {
-                        initWith(getByName("release"))
-                    }
-                    all {
-                        if (name.lowercase().endsWith("debug")) {
-                            setMatchingFallbacks("debug")
-                        } else {
-                            setMatchingFallbacks("release")
+            extensions.configure<KotlinMultiplatformExtension> {
+                androidTarget {
+                    compilations.all {
+                        kotlinOptions {
+                            jvmTarget = "11"
                         }
                     }
                 }
-                compileOptions {
-                    sourceCompatibility = JAVA_SOURCE_COMPAT
-                    targetCompatibility = JAVA_TARGET_COMPAT
-                }
-                kotlinCompile {
-                    kotlinOptions {
-                        jvmTarget = JVM_TARGET
+
+                listOf(
+                    iosX64(),
+                    iosArm64(),
+                    iosSimulatorArm64()
+                ).forEach { iosTarget ->
+                    iosTarget.binaries.framework {
+                        baseName = "ComposeApp"
+                        isStatic = true
                     }
                 }
 
+            }
+            extensions.configure<ApplicationExtension> {
+                compileSdk = COMPILE_SDK
+
+                sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+                sourceSets["main"].res.srcDirs("src/androidMain/res")
+                sourceSets["main"].resources.srcDirs("src/commonMain/resources")
+
+                defaultConfig {
+                    minSdk = MIN_SDK
+                    targetSdk = TARGET_SDK
+                }
                 packaging {
                     resources {
-                        excludes += EXCLUDED_RESOURCES
+                        excludes += "/META-INF/{AL2.0,LGPL2.1}"
                     }
+                }
+                buildTypes {
+                    getByName("release") {
+                        isMinifyEnabled = false
+                    }
+                }
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_11
+                    targetCompatibility = JavaVersion.VERSION_11
                 }
             }
         }
