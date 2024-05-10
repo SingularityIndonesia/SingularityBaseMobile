bd1b2ed7bedf7cdfc6ef5c1dbeed2d87e7d117db
stefanus ayudha <stefanus.ayudha@gmail.com>
2024-05-10 12:07:25 +0700

    chore: add Json module.

--- Shared/common/build.gradle.kts
+++ Shared/common/build.gradle.kts
@@ -34,6 +34,8 @@ kotlin {
             // ktor
             implementation(libs2.ktor.client.core)
             implementation(libs2.ktor.client.cio)
+
+            implementation(libs2.kotlinx.serialization.json)
         }
         iosMain.dependencies {
             implementation(libs2.ktor.client.ios)
