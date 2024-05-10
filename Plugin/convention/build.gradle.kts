f4a335a7477927dbc027bbdd27db8d10ea672917
stefanus ayudha <stefanus.ayudha@gmail.com>
2024-05-10 15:59:20 +0700

    chore: force plugin tobe compiled using java 11.

--- Plugin/convention/build.gradle.kts
+++ Plugin/convention/build.gradle.kts
@@ -10,8 +10,12 @@ plugins {
 group = "com.singularityindonesia.convention"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
 }
 
 dependencies {
