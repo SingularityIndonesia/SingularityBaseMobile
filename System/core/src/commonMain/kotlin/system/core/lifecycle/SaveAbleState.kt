11e3d08da65642f8d7cc34cadc00d6db7baec4b7
stefanus ayudha <stefanus.ayudha@gmail.com>
2024-05-12 20:08:49 +0700

    chore: suppress deprecation.

--- System/core/src/commonMain/kotlin/system/core/lifecycle/SaveAbleState.kt
+++ System/core/src/commonMain/kotlin/system/core/lifecycle/SaveAbleState.kt
@@ -5,10 +5,12 @@ class SaveAbleState {
     @Deprecated("Do not use this directly!")
     val state = mutableListOf<Any>()
 
+    @Suppress("DEPRECATION")
     fun <T : Any> push(state: T) {
         this.state.add(state)
     }
 
+    @Suppress("DEPRECATION")
     inline fun <reified T : Any> pop(): T? {
         val state = this.state.firstOrNull {
             it is T
