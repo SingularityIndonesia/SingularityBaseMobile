e90f4874677e2dcab56e5ffe267391b7b6afd6e8
stefanus ayudha <stefanus.ayudha@gmail.com>
2024-05-16 02:02:51 +0700

    feat(PostmanClientGenerator): wip.

--- Plugin/convention/src/main/kotlin/plugin/convention/postmanclientgenerator/ClientGeneratorStrategy.kt
+++ Plugin/convention/src/main/kotlin/plugin/convention/postmanclientgenerator/ClientGeneratorStrategy.kt
@@ -3,8 +3,6 @@ package plugin.convention.postmanclientgenerator
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonArray
-import kotlinx.serialization.json.boolean
-import kotlinx.serialization.json.double
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
@@ -14,7 +12,7 @@ import plugin.convention.companion.removeNonAlphaNumeric
 import plugin.convention.companion.resolveType
 import java.util.regex.Pattern
 
-fun template(
+fun clientTemplate(
     functionName: String,
     method: String,
     headerModelName: String?,
@@ -31,7 +29,7 @@ fun template(
     val requestContent = requestModel?.print()
     val responseContent = responseModel.print()
 
-    return listOf(
+    return listOfNotNull(
         """
 import io.ktor.client.HttpClient
 import io.ktor.client.request.*
@@ -97,7 +95,6 @@ suspend fun $functionName(
         requestContent?.let { "\n\n$it" },
         responseContent.let { "\n\n$it" }
     )
-        .filterNotNull()
         .fold("") { acc, v -> "$acc$v" }
 }
 
@@ -343,7 +340,7 @@ sealed interface ClientGeneratorStrategy {
     ): PostmanClient
 }
 
-object CommonClientGeneratorV2 : ClientGeneratorStrategy {
+object CommonClientGenerator : ClientGeneratorStrategy {
     override fun generateClient(
         contexts: List<Context>,
         name: String,
@@ -411,7 +408,7 @@ object CommonClientGeneratorV2 : ClientGeneratorStrategy {
                 headers = headers
             )
 
-        val content = template(
+        val content = clientTemplate(
             functionName = name,
             method = method,
             headerModelName = headerModelName,
@@ -428,217 +425,6 @@ object CommonClientGeneratorV2 : ClientGeneratorStrategy {
             name = name,
             content = content
         )
-    }
-}
-
-object CommonClientGenerator : ClientGeneratorStrategy {
-    override fun generateClient(
-        contexts: List<Context>,
-        name: String,
-        request: Postman.Request,
-        response: Postman.ResponseItem
-    ): PostmanClient {
-        val url = request.url
-
-        /**
-         * ```
-         * "path": [
-         * 	"books",
-         * 	":bookID",
-         * 	"comments"
-         * ]
-         * Into : books/${bookID}/comments
-         */
-        val path = url?.path
-            ?.filterNotNull()
-            ?.map {
-                if (it.contains(":")) {
-                    val pureName = it.replaceFirstChar { "" }
-                    "\${$pureName}"
-                } else {
-                    it
-                }
-            }
-            ?.fold("") { acc, v -> "$acc/$v" }
-            ?: throw IllegalArgumentException("path is null:\n $request")
-
-        val method = request.method
-            ?: throw NullPointerException("method is null:\n$request")
-
-        val requestModelName = "${name}Request"
-        val requestModel =
-            RequestModel(
-                name = requestModelName,
-                request = request
-            ).print()
-
-        val responseModelName = "${name}Response"
-        val responseModel =
-            ResponseModel(
-                name = responseModelName,
-                response = response
-            ).print()
-
-        val headers = request.header?.filterNotNull() ?: listOf()
-        val headerModelName = "${name}Header"
-
-        val headerModel =
-            Header(
-                name = headerModelName,
-                headers = headers
-            ).print()
-
-        val function = generateFunction(
-            functionName = name,
-            method = method,
-            headerModelName = if (headerModel != null) headerModelName else null,
-            requestModelName = if (requestModel != null) requestModelName else null,
-            responseModelName = responseModelName,
-            endpoint = path
-        )
-
-        val dependencies = """
-            
-            import io.ktor.client.HttpClient
-            import io.ktor.client.request.*
-            import io.ktor.client.statement.bodyAsText
-            import kotlinx.coroutines.Dispatchers
-            import kotlinx.coroutines.IO
-            import kotlinx.serialization.Serializable
-            import kotlinx.serialization.SerialName
-            import kotlinx.coroutines.withContext
-            import kotlinx.serialization.json.Json
-            import kotlinx.serialization.json.encodeToJsonElement
-            import kotlinx.serialization.json.jsonObject
-            import kotlinx.serialization.json.jsonPrimitive
-        """.trimIndent()
-
-        val final = dependencies
-            .plus("\n\n$function")
-            .plus(headerModel?.let { "\n\n$it" } ?: "")
-            .plus(requestModel?.let { "\n\n$it" } ?: "")
-            .plus("\n\n$responseModel")
-
-        return PostmanClient(
-            name = name,
-            content = final
-        )
-    }
-
-    private fun generateFunction(
-        functionName: String,
-        method: String,
-        headerModelName: String?,
-        requestModelName: String?,
-        responseModelName: String,
-        endpoint: String
-    ): String {
-
-        val pathArguments = run {
-            val pattern = Pattern.compile("""\{(.*?)\}""")
-            val matcher = pattern.matcher(endpoint)
-            val results = mutableListOf<String>()
-
-            while (matcher.find()) {
-                results.add(matcher.group(1))
-            }
-
-            results
-        }
-
-        val pt1 = """
-        suspend fun $functionName(
-            httpClient: HttpClient,
-    """.trimIndent()
-
-        val pt2 = pathArguments
-            .joinToString(",\n") {
-                "\t$it: String"
-            }
-            .let {
-                if (it.isNotEmpty())
-                    "\n$it,"
-                else
-                    it
-            }
-
-        val pt3 = if (headerModelName != null)
-            "\n\theader: $headerModelName,"
-        else
-            ""
-
-        val pt4 = if (requestModelName != null)
-            "\n\trequest: $requestModelName,"
-        else ""
-
-        val pt5 =
-            """
-        
-                ): Result<$responseModelName> = withContext(Dispatchers.IO) {
-                    kotlin.runCatching {
-        
-                        val response = httpClient
-                            .${method.lowercase()}("$endpoint")
-            """.trimIndent()
-
-        val pt6 = if (requestModelName == null && headerModelName == null)
-            ""
-        else {
-            val x1 =
-                """
-                    {
-                        url {
-                """.trimIndent()
-                    .replace("\n", "\n\t\t\t")
-            val x2 = if (headerModelName == null)
-                ""
-            else
-                """
-                    
-                    headers {
-                        Json.encodeToJsonElement(header).jsonObject.forEach { (key, value) ->
-                            append(key, value.jsonPrimitive.content)
-                        }
-                    }
-                """.trimIndent()
-                    .replace("\n", "\n\t\t\t\t\t")
-
-            val x3 = if (requestModelName == null)
-                ""
-            else
-                """
-                    
-                    Json.encodeToJsonElement(request).jsonObject
-                        .forEach {
-                            parameters.append(it.key, it.value.jsonPrimitive.content)
-                        }
-                """.trimIndent()
-                    .replace("\n", "\n\t\t\t\t\t")
-
-            val x4 =
-                """
-                    
-                        }
-                    }
-                """.trimIndent()
-                    .replace("\n", "\n\t\t\t")
-
-            "$x1$x2$x3$x4"
-        }
-
-        val pt7 = """
-
-
-                response
-                    .bodyAsText()
-                    .let {
-                        Json.decodeFromString<$responseModelName>(it)
-                    }
-            }
-        }
-    """.trimIndent()
-
-        return "$pt1$pt2$pt3$pt4$pt5$pt6$pt7"
     }
 }
 
