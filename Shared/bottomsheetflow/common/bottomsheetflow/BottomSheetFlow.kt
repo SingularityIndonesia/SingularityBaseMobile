31ce4faa4873112b05e9abf11da3b18e201c4ffc
stefanus ayudha <stefanus.ayudha@gmail.com>
2024-06-28 07:27:18 +0700

    doc: BottomSheetFlow

--- Shared/bottomsheetflow/common/bottomsheetflow/BottomSheetFlow.kt
+++ Shared/bottomsheetflow/common/bottomsheetflow/BottomSheetFlow.kt
@@ -13,7 +13,18 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
+/**
+ * Configuration scope for the BottomSheetFlow.
+ * @see BottomSheetFlow
+ */
 interface BottomSheetFlowScope {
+
+    /**
+     * Adding route to the flow.
+     * @param route is the route id
+     * @param onDismiss Return true = dismiss bottomsheet. Return false = prevent dismissal. When the user touch the gray area or drag the bottomsheet to the bottom with dismissal intention, the `onDismiss` event will be invoked.
+     * @param content is the composable content.
+     */
     @OptIn(ExperimentalMaterial3Api::class)
     fun route(
         route: String,
@@ -22,12 +33,18 @@ interface BottomSheetFlowScope {
     )
 }
 
+/**
+ * BottomSheet flow is a flow of bottom sheet. You can configure multiple bottomsheets and perform navigation to each bottomsheet.
+ * @param startDestination first launch route.
+ * @param onCancel When the user performs a back action (either through navigation or dismissal) and the navigation reaches the end of the stack, the flow will be canceled, and `onCancel` will be invoked.
+ * @param configuration Configuration scope to configure the bottom sheet.
+ */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BottomSheetFlow(
     startDestination: String,
     onCancel: () -> Unit,
-    builder: BottomSheetFlowScope.() -> Unit,
+    configuration: BottomSheetFlowScope.() -> Unit,
 ) {
     val navController = rememberNavController()
 
@@ -71,6 +88,6 @@ fun BottomSheetFlow(
             }
         }
 
-        builder.invoke(scope)
+        configuration.invoke(scope)
     }
 }
